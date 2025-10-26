73da0d98ec7b02954a2733e0007185780eec1205
Gabriel Melo <gabriel.melo02@gmail.com>
2025-10-26 20:29:34 -0300

    fix: fixing main route

--- api/index.ts
+++ api/index.ts
@@ -4,14 +4,14 @@ import cors from "cors";
 import mainRouter from "../src/routes";
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Middlewares essenciais
 app.use(cors());
 app.use(express.json());
 
 // Rota principal da API
-app.use("/api", mainRouter);
+app.use("/", mainRouter);
 
 if (process.env.VERCEL_ENV !== "production") {
   app.listen(port, () => {
