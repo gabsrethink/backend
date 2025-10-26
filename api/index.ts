3c83050198ea660daf51eecdeb2bd7003da1a62d
Gabriel Melo <gabriel.melo02@gmail.com>
2025-10-26 20:52:53 -0300

    fix: updating CORS options

--- api/index.ts
+++ api/index.ts
@@ -6,8 +6,28 @@ import mainRouter from "../src/routes";
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
-// Middlewares essenciais
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://frontend-taupe-95.vercel.app",
+];
+
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      console.error("Origem não permitida pelo CORS:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+// Middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Rota principal da API
